f86fe39dfd022a57667e8c6eb5bc4c69062d9d71
ianlauyin <ianlauyin@gmail.com>
2024-01-22 21:25:57 +0800

    Add function of get new product

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -1,11 +1,14 @@
 class ProductController {
   constructor(db) {
     this.product = db.product;
+    this.newproduct = db.newproduct;
   }
 
   getNewProduct = async (req, res) => {
     try {
-      const newProduct = await this.product.findAll();
+      const newProduct = await this.newproduct.findAll({
+        include: this.product,
+      });
       return res.json(newProduct);
     } catch (error) {
       return res.status(400).json({ error: true, msg: error });
