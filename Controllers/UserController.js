d4aefc6ff0390ce81e6f7b8dcdcb295c8a5835d5
ianlauyin <ianlauyin@gmail.com>
2024-01-31 13:56:49 +0800

    updated get UserInfo

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,12 +1,19 @@
 class UserController {
   constructor(db) {
     this.user = db.user;
+    this.level = db.level;
   }
 
   getUserInfo = async (req, res) => {
     const { userId } = req.params;
     try {
-      const userInfo = await this.user.findByPk(userId);
+      const userInfo = await this.user.findByPk(userId, {
+        attributes: ["email", "points"],
+        include: {
+          model: this.level,
+          attributes: ["title", "discount", "requirement"],
+        },
+      });
       return res.json(userInfo);
     } catch (error) {
       return res.status(400).send({ error: true, msg: error });
