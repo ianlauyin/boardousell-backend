55215587b9152325e8249167803d5d169baa8106
ianlauyin <ianlauyin@gmail.com>
2024-02-06 15:54:00 +0800

    Update updateUser points will also change the levelId

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -20,10 +20,23 @@ class UserController {
     const { userId } = req.params;
     const newData = req.body;
     try {
-      const user = await this.user.update(newData, {
+      if ("points" in newData) {
+        const levelInfo = await this.level.findAll({
+          attributes: ["id", "requirement"],
+          order: [["requirement", "DESC"]],
+        });
+        newData.levelId = 1;
+        for (const { id, requirement } of levelInfo) {
+          if (newData.points >= requirement) {
+            newData.levelId = id + 1;
+            break;
+          }
+        }
+      }
+      await this.user.update(newData, {
         where: { id: userId },
       });
-      return res.json(user);
+      return res.json("Update Okay");
     } catch (error) {
       return res.status(400).send({ error: true, msg: error });
     }
@@ -51,6 +64,8 @@ class UserController {
       const users = await this.user.findAll({
         where: query,
         attributes: { exclude: ["uuid"] },
+        order: [["updatedAt", "DESC"]],
+        include: this.level,
       });
       return res.json(users);
     } catch (error) {
