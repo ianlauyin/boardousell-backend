8839d48bc1a1bf0fcb8cb1c1e80b52fc437ace79
ianlauyin <ianlauyin@gmail.com>
2024-02-09 00:34:49 +0800

    Add photo delete and change for notices

--- Controllers/NoticeController.js
+++ Controllers/NoticeController.js
@@ -3,6 +3,38 @@ class NoticeController {
     this.notice = db.notice;
   }
 
+  deletePhoto = async (req, res) => {
+    const { noticeId } = req.params;
+    if (isNaN(Number(noticeId))) {
+      return res
+        .status(400)
+        .json({ error: true, msg: "Wrong Type of noticeId" });
+    }
+    try {
+      await this.notice.update({ url: null }, { where: { id: noticeId } });
+      const data = await this.notice.findByPk(noticeId);
+      return res.json(data);
+    } catch (error) {
+      return res.status(400).json({ error: true, msg: error });
+    }
+  };
+
+  changePhoto = async (req, res) => {
+    const { noticeId, url } = req.body;
+    if (isNaN(Number(noticeId))) {
+      return res
+        .status(400)
+        .json({ error: true, msg: "Wrong Type of noticeId" });
+    }
+    try {
+      await this.notice.update({ url: url }, { where: { id: noticeId } });
+      const data = await this.notice.findByPk(noticeId);
+      return res.json(data);
+    } catch (error) {
+      return res.status(400).json({ error: true, msg: error });
+    }
+  };
+
   getAllNotices = async (req, res) => {
     try {
       const allNotices = await this.notice.findAll({
--- Routers/NoticeRouter.js
+++ Routers/NoticeRouter.js
@@ -7,10 +7,15 @@ class NoticeRouter {
   }
 
   routes() {
+    router.put(`/photo`, this.controller.changePhoto.bind(this.controller));
     router.get("/", this.controller.getAllNotices.bind(this.controller));
     router.get(
       "/newest",
       this.controller.getNewestNotices.bind(this.controller)
+    );
+    router.delete(
+      `/photo/:noticeId`,
+      this.controller.deletePhoto.bind(this.controller)
     );
     return router;
   }
