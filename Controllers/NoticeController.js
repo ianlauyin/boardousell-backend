42e32bf396a483a2b6f597fe5fd55952274b9eca
ianlauyin <ianlauyin@gmail.com>
2024-02-09 14:36:08 +0800

    Added AddNotice

--- Controllers/NoticeController.js
+++ Controllers/NoticeController.js
@@ -3,6 +3,47 @@ class NoticeController {
     this.notice = db.notice;
   }
 
+  addNotice = async (req, res) => {
+    const newInfo = req.body;
+    try {
+      const data = await this.notice.create(newInfo);
+      return res.json(data);
+    } catch (error) {
+      return res.status(400).json({ error: true, msg: error });
+    }
+  };
+
+  deleteNotice = async (req, res) => {
+    const { noticeId } = req.params;
+    if (isNaN(Number(noticeId))) {
+      return res
+        .status(400)
+        .json({ error: true, msg: "Wrong Type of noticeId" });
+    }
+    try {
+      await this.notice.destroy({ where: { id: noticeId } });
+      return res.json("Deleted");
+    } catch (error) {
+      return res.status(400).json({ error: true, msg: error });
+    }
+  };
+
+  updateNotice = async (req, res) => {
+    const { noticeId, ...newInfo } = req.body;
+    if (isNaN(Number(noticeId))) {
+      return res
+        .status(400)
+        .json({ error: true, msg: "Wrong Type of noticeId" });
+    }
+    try {
+      await this.notice.update(newInfo, { where: { id: noticeId } });
+      const data = await this.notice.findByPk(noticeId);
+      return res.json(data);
+    } catch (error) {
+      return res.status(400).json({ error: true, msg: error });
+    }
+  };
+
   deletePhoto = async (req, res) => {
     const { noticeId } = req.params;
     if (isNaN(Number(noticeId))) {
